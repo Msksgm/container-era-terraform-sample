936f198d136eb489e496087841617014e563376b
Msksgm <msksgm3124@gmail.com>
2021-09-20 09:30:34 +0900

    add: rdsのリソースを作成

--- service/aws_security_group.tf
+++ service/aws_security_group.tf
@@ -36,9 +36,25 @@ resource "aws_security_group" "alb" {
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db" {
+  name        = "sample-db"
+  description = "DB"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+
+    security_groups = [
+      aws_security_group.instance.id
+    ]
   }
 }
